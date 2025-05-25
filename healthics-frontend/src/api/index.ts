be9cc1f5cde4ea9391392356189da70c6ff74e31
abdes <abdessamadjaouad0@gmail.com>
2025-05-25 01:24:30 +0100

    advisor included

--- healthics-frontend/src/api/index.ts
+++ healthics-frontend/src/api/index.ts
@@ -3,3 +3,4 @@ export { default as authService } from './authService';
 export { default as patientService } from './patientService';
 export { default as documentService } from './documentService';
 export { default as adminService } from './adminService';
+export { default as healthAdvisorService } from './healthAdvisorService';
